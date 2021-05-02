18978f17af59db6cfa96c76e7413be680cdebcc7
Alam Rodriguez <alamgrodriguez@gmail.com>
2021-05-02 00:50:42 -0500

    Visual change on the Taco asset
    [Improve] The Taco now has a part that helps to know the direction it is facing.

--- Assets/Scripts/Taco.cs
+++ Assets/Scripts/Taco.cs
@@ -4,46 +4,34 @@ using UnityEngine;
 
 public class Taco : MonoBehaviour
 {
-    public Rigidbody whiteBallRb;
-    public float shootForce;
+    public Rigidbody whiteBallRb; //Rigidbody de la bola blanca
+    public float shootForce; //Fuerza de tiro
 
-    private Camera cam;
+    private Camera cam; //Main Camera
 
-    IEnumerator Start()
+    void Start()
     {
-        cam = Camera.main;
-        yield return new WaitForSeconds(1f);
+        cam = Camera.main;       
         //whiteBall.GetComponent<Rigidbody>().AddForce(whiteBall.transform.forward.normalized * shootForce, ForceMode.Force);
-        print("Game start :D");
+        print("Game started :D");
     }
 
     void Update()
     {
-        transform.LookAt(whiteBallRb.transform, Vector3.up);
+        transform.LookAt(whiteBallRb.transform, Vector3.up); //Mirar con el taco hacia la bola blanca
         
         Ray ray = cam.ScreenPointToRay(Input.mousePosition); //ScreenPointToRay convierte una coordenada de la pantalla a un rayo
         //Dispara el rayo
         if (Physics.Raycast(ray, out RaycastHit hit))
         {
-            transform.position = new Vector3(hit.point.x, 1f, hit.point.z);
+            transform.position = new Vector3(hit.point.x, 1f, hit.point.z); //Mueve el taco a la posición del mouse, pero Y siempre es fija
         }
 
         if (Input.GetMouseButtonDown(0))
         {
-            //Arreglar el vector, seguramente con inverse matrix se arregle.
-            whiteBallRb.velocity = Vector3.zero;
-            whiteBallRb.transform.rotation = transform.rotation;
-            whiteBallRb.AddForce(whiteBallRb.transform.forward.normalized * shootForce, ForceMode.Force);
-
-            //whiteBallRb.transform.LookAt(transform, Vector3.up);
-
-            //direction = transform.worldToLocalMatrix.inverse * direction;
-
-            //whiteBallRb.transform.rotation = Quaternion.Inverse(whiteBallRb.transform.rotation);
-
-            //whiteBallRb.AddForce(whiteBallRb.transform.forward.normalized * shootForce, ForceMode.Force);
+            whiteBallRb.velocity = Vector3.zero; //Ponemos el velocity de la bola blanca en 0 cada que tiramos
+            whiteBallRb.transform.rotation = transform.rotation; //rotar la bola en la misma dirección que apunta el taco
+            whiteBallRb.AddForce(whiteBallRb.transform.forward.normalized * shootForce * whiteBallRb.mass, ForceMode.Force);
         }
-
-
     }
 }
