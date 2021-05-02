22ae700695ceed775aae6bd26614de4c3489bff1
Alam Rodriguez <alamgrodriguez@gmail.com>
2021-05-02 00:22:56 -0500

    Shoot the white ball
    [Improve] Added a new input to hit the white ball to a desired direction (Mouse left click).

--- Assets/Scripts/Taco.cs
+++ Assets/Scripts/Taco.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 
 public class Taco : MonoBehaviour
 {
-    public GameObject whiteBall;
-    public Transform targetPos;
+    public Rigidbody whiteBallRb;
     public float shootForce;
 
     private Camera cam;
@@ -14,22 +13,37 @@ public class Taco : MonoBehaviour
     {
         cam = Camera.main;
         yield return new WaitForSeconds(1f);
-        whiteBall.GetComponent<Rigidbody>().AddForce(whiteBall.transform.forward.normalized * shootForce, ForceMode.Force);
+        //whiteBall.GetComponent<Rigidbody>().AddForce(whiteBall.transform.forward.normalized * shootForce, ForceMode.Force);
+        print("Game start :D");
     }
 
     void Update()
     {
+        transform.LookAt(whiteBallRb.transform, Vector3.up);
+        
         Ray ray = cam.ScreenPointToRay(Input.mousePosition); //ScreenPointToRay convierte una coordenada de la pantalla a un rayo
         //Dispara el rayo
         if (Physics.Raycast(ray, out RaycastHit hit))
         {
             transform.position = new Vector3(hit.point.x, 1f, hit.point.z);
-            print("ESTOY ENTRANDO WEEEEE");
         }
 
-        transform.LookAt(whiteBall.transform, Vector3.up);
-        
-        //transform.RotateAround(whiteBall.transform.position, Vector3.up, 20 * Time.deltaTime);
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Arreglar el vector, seguramente con inverse matrix se arregle.
+            whiteBallRb.velocity = Vector3.zero;
+            whiteBallRb.transform.rotation = transform.rotation;
+            whiteBallRb.AddForce(whiteBallRb.transform.forward.normalized * shootForce, ForceMode.Force);
+
+            //whiteBallRb.transform.LookAt(transform, Vector3.up);
+
+            //direction = transform.worldToLocalMatrix.inverse * direction;
+
+            //whiteBallRb.transform.rotation = Quaternion.Inverse(whiteBallRb.transform.rotation);
+
+            //whiteBallRb.AddForce(whiteBallRb.transform.forward.normalized * shootForce, ForceMode.Force);
+        }
+
 
     }
 }
