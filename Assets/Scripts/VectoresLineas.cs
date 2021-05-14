a2ed7f0e24dab5e8c94cc71bd2a1ce7410f405b4
Seegikan <62976082+Seegikan@users.noreply.github.com>
2021-05-14 00:28:08 -0500

    Inicio logica de hoyos

--- Assets/Scripts/VectoresLineas.cs
+++ Assets/Scripts/VectoresLineas.cs
@@ -6,84 +6,101 @@ public class VectoresLineas : MonoBehaviour
 {
     [SerializeField]
     private LineRenderer lineRenderer;
-   // private Transform dirBola;
     [SerializeField]
     private float maxDistancia;
     public LayerMask Bola;
     public LayerMask BolaBlanca;
-
-    //public LayerMask bolaPropia;
+    public LayerMask SinBolaBlanca;
 
     public Transform[] hoyos;
-    public Transform posMax;
+    private Transform hoyoCercano;
+    //public Transform posMax;
+
+    private void Update()
+    {
+        
+    }
 
     private void FixedUpdate()
     {
         Ray ray = new Ray(transform.position, transform.TransformDirection(Vector3.forward)); 
 
-        // Does the ray intersect any objects excluding the player layer
-        if (Physics.Raycast(ray, out RaycastHit hit, maxDistancia, Bola))
+        if (Physics.Raycast(ray, out RaycastHit hit, maxDistancia, BolaBlanca))//de Palo a bola blanca
         {
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.cyan);
-            //Debug.Log("Did Hit");
             lineRenderer.positionCount++; //la pelota+++++++++++2
 
             lineRenderer.SetPosition(0, transform.position); //inicio
             lineRenderer.SetPosition(1, hit.point);  //a pelota
-            //lineRenderer.positionCount--;
-            //Ray ray2 = new Ray(hit.transform.position, transform.TransformDirection(Vector3.forward));
 
-            //if (Physics.Raycast(ray2, out RaycastHit hit2, maxDistancia, Bola))
-            if (Physics.SphereCast(hit.transform.position, 0.5f, transform.TransformDirection(Vector3.forward), out RaycastHit hit2, maxDistancia, Bola))
+            if (Physics.SphereCast(hit.transform.position, 0.42f, transform.TransformDirection(Vector3.forward), out RaycastHit hit2, maxDistancia, Bola))//De bola blanca a Bolas
             {
                 Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit2.distance, Color.red);
-                lineRenderer.positionCount++;
-                lineRenderer.positionCount++;
-                lineRenderer.positionCount++;
+                /* lineRenderer.positionCount++;
+                 lineRenderer.positionCount++;
+                 lineRenderer.positionCount++;*/
+                lineRenderer.positionCount = 5;
+                HoyoMasCercano(hit.transform, hoyoCercano);
 
                 lineRenderer.SetPosition(2, hit2.point);
                 lineRenderer.SetPosition(3, hit2.transform.position);
-                lineRenderer.SetPosition(4, hoyos[0].position);
+                lineRenderer.SetPosition(4, hoyoCercano.position);
 
-                //print(hit2.transform.position);
-                CalculoVector90(hoyos[0], hit2.transform, posMax);
-                //posMax.position = hit2.transform.position;
+                CalculoVector90(hoyos[0], hit2.transform);
             }
             else
                 lineRenderer.positionCount = 2;
-            
         }
         else
             lineRenderer.positionCount = 1;
     }
 
-    void CalculoVector90(Transform _hoyoOrigin, Transform _bolaTarget, Transform _posMax)
+    void CalculoVector90(Transform _hoyoOrigin, Transform _bolaTarget)
     {
-        //Vector3 directionVector;
-       // Ray ray = new Ray(_hoyoOrigin.position, _bolaTarget.position - _hoyoOrigin.position);
-
         Debug.DrawRay(_hoyoOrigin.position, _bolaTarget.position - _hoyoOrigin.position, Color.blue);
 
 
-        if ((Physics.Raycast(_hoyoOrigin.transform.position,  _bolaTarget.position - _hoyoOrigin.position, out RaycastHit hit, maxDistancia, Bola)))
+        if ((Physics.Raycast(_hoyoOrigin.transform.position,  _bolaTarget.position - _hoyoOrigin.position, out RaycastHit hit, maxDistancia, Bola)))// de Hoyo a bola
         {
             //Debug.DrawRay(_hoyoOrigin.position, _bolaTarget.TransformDirection(Vector3.forward) * hit.distance, Color.black);
 
-            //directionVector = (_bolaTarget.position - hit.point).normalized;
-           //_posMax.position = directionVector;
-           // print("ya se movio el max " + directionVector );
 
-            if(Physics.Raycast(hit.transform.position, hit.normal, out RaycastHit hitBorde, maxDistancia))
+            if(Physics.Raycast(hit.transform.position, Vector3.Reflect(hit.normal, hit.normal), out RaycastHit hitBorde, maxDistancia, SinBolaBlanca)) //De bola tocada a pared
             {
-                Debug.DrawRay(hit.transform.position, Vector3.Reflect(hit.normal, hit.normal) * hitBorde.distance, Color.yellow);
+                Debug.DrawRay(hit.transform.position, Vector3.Reflect(hit.normal, hit.normal)  * 35f, Color.yellow);
+                //lineRenderer.SetPosition(5, hitBorde.point);
+                lineRenderer.positionCount = 7;
 
-                if(Physics.Raycast(hitBorde.point, hitBorde.point -  _bolaTarget.position , out RaycastHit hitReversa, maxDistancia, Bola))
-                {
-                    //es ese papi debe de apuntar a la ultima bola
-                    Debug.DrawRay(hitBorde.point, hitBorde.point - _bolaTarget.position * hitReversa.distance, Color.red);
+                lineRenderer.SetPosition(5, hit.point);
+                lineRenderer.SetPosition(6, hitBorde.point * 0.1f);
 
-                }
+
+                /* if(Physics.Raycast(hitBorde.point,  _bolaTarget.position , out RaycastHit hitReversa, maxDistancia, Bola)) //de pared a bola
+                 {
+                     //es ese papi debe de apuntar a la ultima bola
+                     Debug.DrawRay(hitBorde.point, _bolaTarget.position * hitReversa.distance, Color.red);
+
+                 }*/
+            }
+        }
+    }//void
+
+
+    void HoyoMasCercano(Transform _peloActual, Transform _hoyoCercano)
+    {
+        float distancia = 0;
+        float tmp;
+
+
+        for (int i = 0; i < hoyos.Length; i++)
+        {
+            tmp = Vector3.Distance(_peloActual.position, hoyos[i].position);
+
+
+            if(distancia <= tmp)
+            {
+                _hoyoCercano = hoyos[i];
             }
         }
     }
-}
+}//class
